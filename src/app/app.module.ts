7df3d26fe20aebc832078bf785a4c1c51b8a217e
Jake Prudent <jake.prudent@gmail.com>
2022-07-11 10:59:22 -0700

    Set up tailwind and started first practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstPracticeAppletonComponent } from './first-practice-appleton/first-practice-appleton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPracticeAppletonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/first-practice-appleton/first-practice-appleton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-practice-appleton',
+  templateUrl: './first-practice-appleton.component.html',
+  styleUrls: ['./first-practice-appleton.component.css']
+})
+export class FirstPracticeAppletonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
